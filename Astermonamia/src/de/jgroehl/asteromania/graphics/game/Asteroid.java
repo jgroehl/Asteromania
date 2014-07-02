57c2171c5e2ed315e33721d61f78a610a9a82f8c
jgroehl <jgroehl@outlook.de>
2014-07-02 07:17:05 +0200

    Asteroids now scale by level.

--- Astermonamia/src/de/jgroehl/asteromania/graphics/game/Asteroid.java
+++ Astermonamia/src/de/jgroehl/asteromania/graphics/game/Asteroid.java
@@ -11,12 +11,36 @@ import de.jgroehl.asteromania.graphics.interfaces.Updatable;
 public class Asteroid extends GraphicsObject implements Updatable, Hitable,
 		Killable {
 
+	private static final int BASIC_LIFE = 2;
+	private static final int BASIC_DAMAGE = 2;
+	private static final float BASIC_SPEED = 0.015f;
+
 	private int life;
 	private int damage;
 	private float speed;
 
 	public enum AsteroidType {
-		SMALL, MEDIUM, LARGE, HUGE, EXTREME;
+		SMALL(0.5f, de.jgroehl.asteromania.R.drawable.rock3), MEDIUM(0.67f,
+				de.jgroehl.asteromania.R.drawable.rock5), LARGE(1f,
+				de.jgroehl.asteromania.R.drawable.rock4), HUGE(1.5f,
+				de.jgroehl.asteromania.R.drawable.rock2), EXTREME(2f,
+				de.jgroehl.asteromania.R.drawable.rock1);
+
+		private final float modifyer;
+		private final int image;
+
+		private AsteroidType(float modifyer, int image) {
+			this.modifyer = modifyer;
+			this.image = image;
+		}
+
+		public float getModifyer() {
+			return modifyer;
+		}
+
+		public int getImage() {
+			return image;
+		}
 	}
 
 	public Asteroid(float xPosition, float yPosition, int graphicsId,
@@ -45,9 +69,18 @@ public class Asteroid extends GraphicsObject implements Updatable, Hitable,
 	}
 
 	public static Asteroid createAsteroid(Context context, int level) {
+		AsteroidType type = getRandomAsteroidType();
 		return new Asteroid((float) Math.random(),
-				(float) (Math.random() * (-2)),
-				de.jgroehl.asteromania.R.drawable.rock5, context, 5, 3, 0.01f);
+				(float) (Math.random() * (-Math.cbrt(level))), type.getImage(),
+				context, (int) Math.round(BASIC_LIFE * type.getModifyer()
+						* Math.cbrt(level)), (int) Math.round(BASIC_DAMAGE
+						* type.getModifyer() * Math.sqrt(level)), BASIC_SPEED
+						/ (float) Math.sqrt(type.getModifyer()));
+	}
+
+	private static AsteroidType getRandomAsteroidType() {
+		return AsteroidType.values()[(int) (Math.random() * AsteroidType
+				.values().length)];
 	}
 
 	@Override
@@ -58,7 +91,10 @@ public class Asteroid extends GraphicsObject implements Updatable, Hitable,
 			gameHandler.remove(shot);
 			if (life <= 0) {
 				gameHandler.remove(this);
-				Coin.addToHandler(gameHandler, this);
+				int amountCoins = (int) Math.round(Math.random()
+						* Math.sqrt(damage)) + 1;
+				for (int i = 0; i < amountCoins; i++)
+					Coin.addToHandler(gameHandler, this);
 			}
 		}
 	}
