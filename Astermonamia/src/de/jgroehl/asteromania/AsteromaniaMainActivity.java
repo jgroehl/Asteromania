938d43a4f0f290e7192f6fda291ba585d6ddcd38
jgroehl <jgroehl@outlook.de>
2014-09-27 11:37:52 +0200

    Included welcome text by Toast

--- Astermonamia/src/de/jgroehl/asteromania/AsteromaniaMainActivity.java
+++ Astermonamia/src/de/jgroehl/asteromania/AsteromaniaMainActivity.java
@@ -1,10 +1,12 @@
 package de.jgroehl.asteromania;
 
 import android.os.Bundle;
+import android.widget.Toast;
 import de.jgroehl.api.activities.AbstractGooglePlayGamesLoginActivity;
 import de.jgroehl.asteromania.control.GoogleApiHandler;
 
-public class AsteromaniaMainActivity extends AbstractGooglePlayGamesLoginActivity {
+public class AsteromaniaMainActivity extends
+		AbstractGooglePlayGamesLoginActivity {
 
 	private AsteromaniaGamePanel mainGamePanel;
 	public static final boolean DEBUG = false;
@@ -12,8 +14,10 @@ public class AsteromaniaMainActivity extends AbstractGooglePlayGamesLoginActivit
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mainGamePanel = new AsteromaniaGamePanel(this, new GoogleApiHandler(getApiClient(), this), this);
-
+		mainGamePanel = new AsteromaniaGamePanel(this, new GoogleApiHandler(
+				getApiClient(), this), this);
+		Toast.makeText(this, "Welcome to Asteromania", Toast.LENGTH_LONG)
+				.show();
 		setContentView(mainGamePanel);
 	}
 
