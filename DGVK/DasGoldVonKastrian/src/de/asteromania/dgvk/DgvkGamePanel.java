bf1e8b70fbe87a193d6c43fd655d7b1b9a518817
jgroehl <jgroehl@outlook.de>
2015-01-22 12:10:22 +0100

    Added DoHttpGetRequest

--- DGVK/DasGoldVonKastrian/src/de/asteromania/dgvk/DgvkGamePanel.java
+++ DGVK/DasGoldVonKastrian/src/de/asteromania/dgvk/DgvkGamePanel.java
@@ -1,15 +1,15 @@
 package de.asteromania.dgvk;
 
-import java.util.Random;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
 
 import android.content.Context;
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.util.Log;
-import de.asteromania.dgvk.dto.ScoreDto;
 import de.jgroehl.api.AbstractGamePanel;
 import de.jgroehl.api.activities.AbstractSimpleActivity;
 import de.jgroehl.api.control.BaseGameHandler;
@@ -43,9 +43,39 @@ public class DgvkGamePanel extends AbstractGamePanel
 			public void action(BaseGameHandler gameHandler)
 			{
 				Log.i(TAG, "Starting HttpPostRequest...");
-				new DoHttpPostTask().execute("http://192.168.0.2:8080/DGVK-Server/rest/score");
+				// new
+				// DoHttpPostTask().execute("http://192.168.0.2:8080/DGVK-Server/rest/score",
+				// new ScoreDto(new Random(
+				// System.currentTimeMillis()).nextLong()).toXml());
+
+				new DoHttpGetTask().execute("http://httpbin.org/ip");
 			}
 		}, getContext()), GameState.STARTING_SCREEN);
+	}
+
+	private class DoHttpGetTask extends AsyncTask<String, Long, String>
+	{
+		private final String TAG = DoHttpGetTask.class.getSimpleName();
+
+		@Override
+		protected String doInBackground(String... params)
+		{
+			try
+			{
+				AndroidHttpClient client = AndroidHttpClient.newInstance("androidClient", getContext());
+				HttpGet get = new HttpGet(params[0]);
+				HttpResponse response = client.execute(get);
+				String string = EntityUtils.toString(response.getEntity());
+				Log.i("RESULT", string);
+				return string;
+			}
+			catch (Exception e)
+			{
+				Log.e(TAG, e.getLocalizedMessage());
+				return null;
+			}
+		}
+
 	}
 
 	private class DoHttpPostTask extends AsyncTask<String, Long, Integer>
@@ -60,8 +90,7 @@ public class DgvkGamePanel extends AbstractGamePanel
 			{
 				AndroidHttpClient client = AndroidHttpClient.newInstance("androidClient", getContext());
 				HttpPost post = new HttpPost(params[0]);
-				String xml = new ScoreDto(new Random(System.currentTimeMillis()).nextLong()).toXml();
-				post.setEntity(new StringEntity(xml));
+				post.setEntity(new StringEntity(params[1]));
 				return client.execute(post).getStatusLine().getStatusCode();
 			}
 			catch (Exception e)
